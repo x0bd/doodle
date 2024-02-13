b759839520e82521370334b40c2bddb020ffb233
Tinodaishe Tembo <synthmancer@gmail.com>
2024-02-13 12:04:02 +0200

    wip hello world

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,18 +3,32 @@ import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
-    },
-    plugins: [react()]
-  }
+	main: {
+		plugins: [externalizeDepsPlugin()],
+		resolve: {
+			alias: {
+				'@lib': resolve('src/main/lib'),
+				'@shared': resolve('src/shared')
+			}
+		}
+	},
+
+	preload: {
+		plugins: [externalizeDepsPlugin()]
+	},
+	renderer: {
+		assetsInclude: 'src/renderer/assets/**',
+		resolve: {
+			alias: {
+				'@renderer': resolve('src/renderer/src'),
+				'@shared': resolve('src/shared'),
+				'@/hooks': resolve('src/renderer/src/hooks'),
+				'@/assets': resolve('src/renderer/src/assets'),
+				'@/store': resolve('src/renderer/src/store'),
+				'@/components': resolve('src/renderer/src/components'),
+				'@/mocks': resolve('src/renderer/src/mocks')
+			}
+		},
+		plugins: [react()]
+	}
 })
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -2,11 +2,11 @@ import { contextBridge } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
 if (!process.contextIsolated) {
-  throw new Error('contextIsolation must be enabled in the browser window')
+	throw new Error('contextIsolation must be enabled in the browser window')
 }
 
 try {
-  contextBridge.exposeInMainWorld('context', {})
+	contextBridge.exposeInMainWorld('context', {})
 } catch (error) {
-  console.log(error)
+	console.log(error)
 }
