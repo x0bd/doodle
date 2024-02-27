b59ec7e4eceb4c28d3fb2a29e8693e9068b31f37
Tinodaishe Tembo <synthmancer@gmail.com>
2024-02-27 22:16:07 +0200

    wip add mdx editor

---
+++ src/renderer/src/components/MarkdownEditor.tsx
@@ -0,0 +1,5 @@
+const MarkdownEditor = () => {
+	return <div>MarkdownEditor</div>;
+};
+
+export default MarkdownEditor;
--- src/renderer/src/components/index.ts
+++ src/renderer/src/components/index.ts
@@ -3,3 +3,4 @@ export * from "./Button";
 export * from "./ActionButtonsRow";
 export * from "./NotePreviewList";
 export * from "./NotePreview";
+export * from "./MarkdownEditor";
