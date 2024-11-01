1cd4f5c331ecc12bbbc6a07c2638f76cd0f704c9
xoboid <112933783+x0bd@users.noreply.github.com>
2024-11-01 23:27:54 +0200

    Revert "move back to next 14"
    This reverts commit 4a1a6e7bf63828c47f9f50b90c0296e2ba0791c0.

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,66 @@
-import Image from "next/image";
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const [isDrawing, setIsDrawing] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	useEffect(() => {
+		const canvas = canvasRef.current;
+
+		if (canvas) {
+			const ctx = canvas.getContext("2d");
+			if (ctx) {
+				canvas.width = window.innerWidth;
+				canvas.height = window.innerHeight - canvas.offsetTop;
+				ctx.lineCap = "round";
+				ctx.lineWidth = 3;
+			}
+		}
+	}, []);
+
+	const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
+		const canvas = canvasRef.current;
+		if (canvas) {
+			canvas.style.background = "black";
+			const ctx = canvas.getContext("2d");
+			if (ctx) {
+				ctx.beginPath();
+				ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+				setIsDrawing(true);
+			}
+		}
+	};
+
+	const stopDrawing = () => {
+		setIsDrawing(false);
+	};
+
+	const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
+		if (!isDrawing) {
+			return;
+		}
+		const canvas = canvasRef.current;
+		if (canvas) {
+			const ctx = canvas.getContext("2d");
+			if (ctx) {
+				ctx.strokeStyle = "white";
+				ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+				ctx.stroke();
+			}
+		}
+	};
+
+	return (
+		<canvas
+			ref={canvasRef}
+			id="canvas"
+			className="absolute top-0 left-0 w-full h-full"
+			onMouseDown={startDrawing}
+			onMouseOut={stopDrawing}
+			onMouseUp={stopDrawing}
+			onMouseMove={draw}
+		/>
+	);
 }
