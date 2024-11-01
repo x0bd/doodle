76967ada16983f5b3a4e3e7fae57246a6951da09
xoboid <112933783+x0bd@users.noreply.github.com>
2024-11-02 00:16:54 +0200

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
+import { SWATCHES } from "@/constants";
+import { ColorSwatch, Group } from "@mantine/core";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+
+interface Response {
+	expr: string;
+	result: string;
+	assign: boolean;
+}
+
+interface GeneratedResult {
+	expression: string;
+	answer: string;
+}
 
 export default function Home() {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	const [isDrawing, setIsDrawing] = useState(false);
+	const [color, setColor] = useState("rgb(255, 255, 255)");
+	const [reset, setReset] = useState(false);
+	const [result, setResult] = useState<GeneratedResult>();
+	const [dictOfVars, setDictOfVars] = useState({});
+
+	useEffect(() => {
+		if (reset) {
+			resetCanvas();
+			setReset(false);
+		}
+	}, [reset]);
 
 	useEffect(() => {
 		const canvas = canvasRef.current;
@@ -37,6 +63,34 @@ export default function Home() {
 		setIsDrawing(false);
 	};
 
+	const resetCanvas = () => {
+		const canvas = canvasRef.current;
+		if (canvas) {
+			const ctx = canvas.getContext("2d");
+			if (ctx) {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+			}
+		}
+	};
+
+	const sendData = async () => {
+		const canvas = canvasRef.current;
+
+		// Needs Further Work Here!!!!
+		if (canvas) {
+			const response = await axios({
+				method: "post",
+				url: "",
+				data: {
+					image: canvas.toDataURL("image/png"),
+					dict_of_vars: dictOfVars,
+				},
+			});
+			const resp = await response.data;
+			console.log("Response");
+		}
+	};
+
 	const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
 		if (!isDrawing) {
 			return;
@@ -45,7 +99,7 @@ export default function Home() {
 		if (canvas) {
 			const ctx = canvas.getContext("2d");
 			if (ctx) {
-				ctx.strokeStyle = "white";
+				ctx.strokeStyle = color;
 				ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
 				ctx.stroke();
 			}
@@ -53,14 +107,16 @@ export default function Home() {
 	};
 
 	return (
-		<canvas
-			ref={canvasRef}
-			id="canvas"
-			className="absolute top-0 left-0 w-full h-full"
-			onMouseDown={startDrawing}
-			onMouseOut={stopDrawing}
-			onMouseUp={stopDrawing}
-			onMouseMove={draw}
-		/>
+		<>
+			<canvas
+				ref={canvasRef}
+				id="canvas"
+				className="absolute top-0 left-0 w-full h-full"
+				onMouseDown={startDrawing}
+				onMouseOut={stopDrawing}
+				onMouseUp={stopDrawing}
+				onMouseMove={draw}
+			/>
+		</>
 	);
 }
